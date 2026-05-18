87d93cbbc601d200cb0385a8b63efb3ae7fb4b12
midori <flower01pear@gmail.com>
2026-05-18 14:46:30 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -7,24 +7,11 @@ window.addEventListener('scroll', () => {
     const position = el.getBoundingClientRect().top;
 
     if(position < window.innerHeight - 100){
+
       el.classList.add('show');
+
     }
 
   });
 
 });
-
-const tags = document.querySelectorAll('.floating-tags span');
-
-setInterval(() => {
-
-  tags.forEach(tag => {
-
-    const x = Math.random() * 20 - 10;
-    const y = Math.random() * 20 - 10;
-
-    tag.style.transform = `translate(${x}px, ${y}px)`;
-
-  });
-
-}, 1500);
